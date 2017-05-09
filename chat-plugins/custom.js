9897800e422bcd54e4f6658d0f503cb4d3cddf6c
theodelhay <theodelhay@orange.fr>
2017-05-09 12:43:44 -0600

    Update custom.js

--- chat-plugins/custom.js
+++ chat-plugins/custom.js
@@ -23,8 +23,36 @@ exports.commands = {
 		}
 		connection.popup(buffer.join("\n\n"));
 	},*/
+		masspm: 'pmall',
+	pmall: function (target, room, user) {
+		if (!this.can('pmall')) return false;
+		if (!target) return this.parse('/help pmall');
+
+		var pmName = 'Annonces Global';
+
+		for (var i in Users.users) {
+			var message = '|pm|' + pmName + '|' + Users.users[i].getIdentity() + '|' + target;
+			Users.users[i].send(message);
+		}
+	},
+	pmallhelp: ["/pmall [message] -Envoie un pm a tout les utilisateurs"],
 	
-	postimage: 'image',
+	staffpm: 'pmallstaff',
+	pmstaff: 'pmallstaff',
+	pmallstaff: function (target, room, user) {
+		if (!this.can('forcewin')) return false;
+		if (!target) return this.parse('/help pmallstaff');
+
+		var pmName = ' Annonces Staff';
+
+		for (var i in Users.users) {
+			if (Users.users[i].isStaff) {
+				Users.users[i].send('|pm|' + pmName + '|' + Users.users[i].group + Users.users[i].name + '|' + target);
+			}
+		}
+	},
+	
+	img: 'image',
 	image: function (target, room, user) {
 		if (!target) return this.sendReply('Usage: /image link, size');
 		if (!this.can('ban', room)) return false;
@@ -48,6 +76,54 @@ exports.commands = {
 		fsscript.writeFileSync(DATA_DIR + "custom.css", target.toString());
 		this.sendReply("custom.css editado correctamente.");
 	},
+	
+	spop: 'sendpopup',
+	sendpopup: function(target, room, user) {
+		if (!this.can('popup')) return false;
+
+		target = this.splitTarget(target);
+		var targetUser = this.targetUser;
+
+		if (!targetUser) return this.sendReply('/sendpopup [user], [message] - Vous avez manqué l utilisateur');
+		if (!target) return this.sendReply('/sendpopup [user], [message] - vous avez manqué le message');
+
+		targetUser.popup(target);
+		this.sendReply(targetUser.name + ' A reçu le message sous forme de popup: ' + target);
+
+		targetUser.send(user.name+' Vous envoye un message.');
+
+		this.logModCommand(user.name+' Envoyer un message à '+targetUser.name);
+	},
+		roomshowimage: 'roomsi',
+	roomsi: function(target, user, room) {
+		if (!this.can('announce')) return;
+		if(!target) return this.errorReply("/roomsi [imageurl]");
+	this.sendReply("|raw|<img src=" + target + ">");
+	},
+	   sudo: function(target, room, user) {
+        if (!user.can('sudo')) return;
+        var parts = target.split(',');
+        if (parts.length < 2) return this.parse('/help sudo');
+        if (parts.length >= 3) parts.push(parts.splice(1, parts.length).join(','));
+        var targetUser = parts[0],
+            cmd = parts[1].trim().toLowerCase(),
+            commands = Object.keys(CommandParser.commands).join(' ').toString(),
+            spaceIndex = cmd.indexOf(' '),
+            targetCmd = cmd;
+
+        if (spaceIndex > 0) targetCmd = targetCmd.substr(1, spaceIndex - 1);
+
+        if (!Users.get(targetUser)) return this.sendReply('User ' + targetUser + ' not found.');
+        if (commands.indexOf(targetCmd.substring(1, targetCmd.length)) < 0 || targetCmd === '') return this.sendReply('Not a valid command.');
+        if (cmd.match(/\/me/)) {
+            if (cmd.match(/\/me./)) return this.parse('/control ' + targetUser + ', say, ' + cmd);
+            return this.sendReply('You must put a target to make a user use /me.');
+        }
+        CommandParser.parse(cmd, room, Users.get(targetUser), Users.get(targetUser).connections[0]);
+        this.sendReply('You have made ' + targetUser + ' do ' + cmd + '.');
+    },
+
+
 	
 	destroymodlog: function (target, room, user, connection) {
 		if (!user.hasConsoleAccess(connection)) {return this.sendReply("/destroymodlog - Access denied.");}
@@ -88,9 +164,9 @@ exports.commands = {
 		if (!this.can('roomlist')) return;
 		var rooms = Object.keys(Rooms.rooms);
 		var len = rooms.length;
-		var official = ['<b><font color="#1a5e00" size="2">Salas oficiales:</font></b><br><br>'];
-		var nonOfficial = ['<hr><b><font color="#000b5e" size="2">Salas no-oficiales:</font></b><br><br>'];
-		var privateRoom = ['<hr><b><font color="#5e0019" size="2">Salas privadas:</font></b><br><br>'];
+		var official = ['<b><font color="#1a5e00" size="2">Salle officiel:</font></b><br><br>'];
+		var nonOfficial = ['<hr><b><font color="#000b5e" size="2">Salle non officiel:</font></b><br><br>'];
+		var privateRoom = ['<hr><b><font color="#5e0019" size="2">Salle privers:</font></b><br><br>'];
 		while (len--) {
 			var _room = Rooms.rooms[rooms[(rooms.length - len) - 1]];
 			if (_room.type === 'chat') {
