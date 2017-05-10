77726019376e9e763b0f5a5e4a9679cccca3fa75
theodelhay <theodelhay@orange.fr>
2017-05-10 10:37:45 -0600

    Update daymute.js

--- chat-plugins/daymute.js
+++ chat-plugins/daymute.js
@@ -5,24 +5,24 @@ exports.commands = {
 	daymute: function (target, room, user) {
 		if (!target) return this.parse('/help daymute');
 		if (!this.can('mute', targetUser, room)) return false;
-                if (room.isMuted(user) && !user.can('bypassall')) return this.errorReply("You cannot do this while unable to talk.");
+                if (room.isMuted(user) && !user.can('bypassall')) return this.errorReply("Vous ne pouvez pas le faire sans pouvoir parler.");
 		
 		target = this.splitTarget(target);
 		var targetUser = this.targetUser;
-		if (!targetUser) return this.sendReply('User "' + this.targetUsername + '" not found.');
+		if (!targetUser) return this.sendReply('User "' + this.targetUsername + '" Introuvable.');
 
 		if ((room.isMuted(targetUser) && !canBeMutedFurther) || targetUser.locked || !targetUser.connected) {
-			var problem = " but was already " + (!targetUser.connected ? "offline" : targetUser.locked ? "locked" : "licked");
+			var problem = " Mais était déjà " + (!targetUser.connected ? "déconnecter" : targetUser.locked ? "locked" : "licked");
 			if (!target) {
-				return this.privateModCommand("(" + targetUser.name + " would be muted by " + user.name + problem + ".)");
+				return this.privateModCommand("(" + targetUser.name + " Vous avez était mute par" + user.name + problem + ".)");
 			}
-			return this.addModCommand("" + targetUser.name + " would be muted by " + user.name + problem + "." + (target ? " (" + target + ")" : ""));
+			return this.addModCommand("" + targetUser.name + " Vous avez était mute par" + user.name + problem + "." + (target ? " (" + target + ")" : ""));
 		}
 
-		targetUser.popup(user.name + ' has muted you for 24 hours. ' + target);
-		this.addModCommand('' + targetUser.name + ' was muted by ' + user.name + ' for 24 hours.' + (target ? " (" + target + ")" : ""));
+		targetUser.popup(user.name + 'Vous a mute pendant 24 heures. ' + target);
+		this.addModCommand('' + targetUser.name + ' A été mute par ' + user.name + ' pendant 24 heures.' + (target ? " (" + target + ")" : ""));
 		var alts = targetUser.getAlts();
-		if (alts.length) this.addModCommand("" + targetUser.name + "'s alts were also muted: " + alts.join(", "));
+		if (alts.length) this.addModCommand("" + targetUser.name + "Ces alts on était mute! " + alts.join(", "));
 
 		room.mute(targetUser, 24 * 60 * 60 * 1000, true);
 	},
